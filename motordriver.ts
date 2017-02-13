dc15b81f3d48bd0c1184370866fbf1fbf58ac0b2
luniki <luniki@gmail.com>
2017-02-13 13:42:40 +0100

    more stuff

---
+++ motordriver.ts
@@ -0,0 +1,10 @@
+//% weight=6 color=#66290F
+namespace motordriver {
+    //% weight=1
+    //% blockId="motordriver_rgb" block="red %red|green %green|blue %blue"
+    //% advanced=true
+    export function rgb(red: number, green: number, blue: number): number {
+        return 1;
+    }
+
+}
